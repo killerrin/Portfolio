af294706c6531d726f667fc2008775d160a196c6
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-06-07 23:32:47 -0400

    Added a LinkTypeController
    Added a convenience Self-Documenting API call that parses the LinkType
    Enum into an IEnumerable and returns that.

---
+++ Portfolio.API/Controllers/LinkTypeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.API.Models;
+using Portfolio.API.Repositories;
+using Portfolio.API.Services;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Cors;
+using Portfolio.API.Extensions;
+using Portfolio.API.Models.Enums;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Portfolio.API.Controllers
+{
+    [Route("api/[controller]")]
+    //[EnableCors("CorsEverythingPolicy")]
+    public class LinkTypeController : Controller
+    {
+        //private readonly IRepository<LinkType> _linkTypeRepository;
+        //private readonly AuthenticationService _authenticationService;
+
+        public LinkTypeController()//IRepository<TagType> linkTypeRepository)
+        {
+            //_linkTypeRepository = linkTypeRepository;
+            //_authenticationService = new AuthenticationService(new UserRepository(linkTypeRepository.DatabaseInfo.Context));
+        }
+
+        [HttpGet]
+        public IEnumerable<EnumModel> GetAll()
+        {
+            return EnumExtensions.GetNameValuePair<LinkType>();
+        }
+
+        [HttpGet("{id}", Name = "GetLinkType")]
+        public IActionResult GetById(int id)
+        {
+            if (Enum.TryParse<LinkType>(id.ToString(), out LinkType linkType))
+            {
+                EnumModel item = new EnumModel(linkType);
+                if (!item.Name.Equals(item.Value.ToString()))
+                    return new ObjectResult(item);
+            }
+            return NotFound();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create([Required][FromHeader(Name = "Authorization")] string authToken, [FromBody] TagType item)
+        //{
+        //    if (!_authenticationService.VerifyAuthToken(authToken))
+        //        return BadRequest("Invalid AuthToken");
+
+        //    if (item == null)
+        //        return BadRequest();
+
+        //    _linkTypeRepository.AddAndCommit(item);
+        //    return CreatedAtRoute("GetLinkType", new { id = item.ID }, item);
+        //}
+
+        //[HttpPut("{id}")]
+        //public IActionResult Update(int id, [Required][FromHeader(Name = "Authorization")] string authToken, [FromBody] TagType item)
+        //{
+        //    if (!_authenticationService.VerifyAuthToken(authToken))
+        //        return BadRequest("Invalid AuthToken");
+
+        //    if (item == null || item.ID != id)
+        //        return BadRequest();
+
+        //    var repoItem = _linkTypeRepository.Find(id);
+        //    if (repoItem == null)
+        //        return NotFound();
+
+        //    repoItem.Name = item.Name;
+
+        //    _linkTypeRepository.UpdateAndCommit(repoItem);
+
+        //    return new NoContentResult();
+        //}
+
+        //[HttpDelete("{id}")]
+        //public IActionResult Delete(int id, [Required][FromHeader(Name = "Authorization")] string authToken)
+        //{
+        //    if (!_authenticationService.VerifyAuthToken(authToken))
+        //        return BadRequest("Invalid AuthToken");
+
+        //    var repoItem = _linkTypeRepository.Find(id);
+        //    if (repoItem == null)
+        //        return NotFound();
+
+        //    _linkTypeRepository.RemoveAndCommit(id);
+        //    return new NoContentResult();
+        //}
+    }
+}
---
+++ Portfolio.API/Extensions/EnumExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio.API.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static IEnumerable<EnumModel> GetNameValuePair<T>()
+        {
+            List<EnumModel> enums = new List<EnumModel>();
+
+            Type genericType = typeof(T);
+            foreach (T obj in Enum.GetValues(genericType))
+            {
+                Enum test = Enum.Parse(typeof(T), obj.ToString()) as Enum;
+                int x = Convert.ToInt32(test);
+                enums.Add(new EnumModel(obj.ToString(), x));
+            }
+
+            return enums;
+        }
+    }
+    public class EnumModel
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public EnumModel(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public EnumModel(Enum e)
+        {
+            Name = e.ToString();
+            Value = Convert.ToInt32(e);
+        }
+    }
+}
